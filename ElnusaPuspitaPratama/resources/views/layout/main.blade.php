00709e21b2080586d814d8794e0997aa36467949
Bryan Carlie Lukito Setiawan <blukitosetiawan@gmail.com>
2025-10-12 13:58:05 +0700

    AOS

--- ElnusaPuspitaPratama/resources/views/layout/main.blade.php
+++ ElnusaPuspitaPratama/resources/views/layout/main.blade.php
@@ -9,11 +9,12 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-        <link rel="stylesheet" href="css/navigation.css">
-        <link rel="stylesheet" href="css/footer.css">
-        <link rel="stylesheet" href="css/cardHover.css">
-        <link rel="stylesheet" href="css/projectCard.css">
-        <link rel="stylesheet" href="css/reviewCard.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.css">
+    <link rel="stylesheet" href="css/navigation.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/cardHover.css">
+    <link rel="stylesheet" href="css/projectCard.css">
+    <link rel="stylesheet" href="css/reviewCard.css">
 </head>
 
 <body class="d-flex flex-column min-vh-100">
@@ -28,6 +29,10 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-PJsz6Ctp1JQ2ChJt2LbHYXHt2MIEl/T0ZJj3QA5NjY5Di6lXzDL2bBLxZqRMfT0W" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.js"></script>
+    <script>
+        AOS.init();
     </script>
 </body>
 
