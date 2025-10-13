13511189661b202891dbd44df0511609b97e8bed
Bryan Carlie Lukito Setiawan <blukitosetiawan@gmail.com>
2025-10-13 07:36:36 +0700

    benerin idkit

--- ElnusaPuspitaPratama/app/Http/Controllers/EmployeeController.php
+++ ElnusaPuspitaPratama/app/Http/Controllers/EmployeeController.php
@@ -16,16 +16,6 @@ class EmployeeController extends Controller
                             ->orderBy('nama') 
                             ->get();
         
-        
-        $management = $employees->filter(function($emp) {
-            return str_contains(strtolower($emp->position ?? ''), 'manager') 
-                || str_contains(strtolower($emp->position ?? ''), 'director');
-        });
-        
-        $engineers = $employees->filter(function($emp) {
-            return str_contains(strtolower($emp->position ?? ''), 'engineer');
-        });
-        
         return view('team', compact('employees', 'management', 'engineers', 'totalEmployee'));
     }
 
