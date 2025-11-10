ff091cff774cfc04d021f171c91a4bb3c9582291
Gee <ssantoso04@student.ciputra.ac.id>
2025-11-10 08:40:00 +0700

    updates

--- ElnusaPuspitaPratama/database/seeders/DatabaseSeeder.php
+++ ElnusaPuspitaPratama/database/seeders/DatabaseSeeder.php
@@ -2,21 +2,18 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+    /**
+     * Seed the application's database.
+     */
     public function run(): void
     {
-        // Buat user admin
-        User::factory()->create([
-            'name' => 'Admin Elnusa',
-            'email' => 'admin@elnusa.com',
-        ]);
-
-        
+        // Call all seeders in the correct order
         $this->call([
+            UserSeeder::class,        
             ClientSeeder::class,      
             EmployeeSeeder::class,    
             ProjectSeeder::class,     
